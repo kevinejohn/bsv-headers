6e1672647e2fe03a31dbb1a56eaf8f4c4aeb447c
Kevin Johnson <kevinejohn@gmail.com>
2021-09-17 12:32:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# bsv-headers
+
+[![NPM Package](https://img.shields.io/npm/v/bsv-headers.svg?style=flat-square)](https://www.npmjs.org/package/bsv-headers)
+
+Determine longest bitcoin chain and query block order
+
+## Note
+
+You must use node.js v12+
+
+## Install
+
+```sh
+npm i bsv-headers
+```
+
+## Use
+
+```js
+const BsvHeaders = require('bsv-headers')
+
+const headers = new BsvHeaders({
+  genesisHash:
+    '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f',
+  invalidBlocks: [],
+  maxReorgDepth: 1000, // How far back to recalculate longest chain after adding new block headers. Set to 0 to always recalculate from genesis (slower)
+})
+
+// const buf = Buffer.from(/* <an 80 byte bitcoin block header> */)
+// headers.addHeader({ buf, /* hash: <Optional: 32 byte buffer or 64 char string hex string of buf block hash. Used for performance> */ })
+// // ...
+// // Add all block headers
+// // ...
+// headers.addHeader({ buf, /* hash: <Optional: 32 byte buffer or 64 char string hex string of buf block hash. Used for performance> */ })
+
+const tip = headers.getTip()
+console.log(`Chain tip: ${tip.height} ${tip.hash}`)
+
+const hash = headers.getHash(0)
+console.log(`Block 0 hash: ${hash}`)
+
+const height = headers.getHeight(
+  '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f',
+)
+console.log(
+  `Block 000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f height: ${height}`,
+)
+
+const header1 = headers.getHeader({
+  hash: '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f',
+})
+const header2 = headers.getHeader({ height: 0 })
+console.log(header1, header2)
+
+headers.invalidateBlock(
+  '00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048',
+)
+console.log(headers.getTip())
+```
+
+## Tests
+
+`TODO`
+
+## Future features
+
+- Validate block difficulty
+
+## License
+
+The license for the code in this repository is the Open BSV License.
---
+++ src/index.js
@@ -0,0 +1,164 @@
+const bsv = require('bsv-minimal')
+
+const GENESIS_BLOCK =
+  '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+
+class Headers {
+  constructor(opts = {}) {
+    const {
+      genesisHash = GENESIS_BLOCK,
+      invalidBlocks = [],
+      maxReorgDepth = 1000
+    } = opts
+    this.genesisHash = genesisHash
+    this.invalidBlocks = new Set(invalidBlocks)
+    this.maxReorgDepth = maxReorgDepth
+    this.headers = {}
+    this.chain = {}
+    this.processed = true
+  }
+
+  addHeader({ buf, hash = false }) {
+    if (hash) hash = hash.toString('hex')
+    if (this.headers[hash]) return this.headers[hash]
+    const header = bsv.Header.fromBuffer(buf)
+    header.hashHex = hash || header.getHash().toString('hex')
+    if (!hash && this.headers[header.hashHex])
+      return this.headers[header.hashHex]
+    if (this.invalidBlocks.has(header.hashHex)) throw Error(`Invalid block`)
+    header.prevHashHex = header.prevHash.toString('hex')
+    header.nextHash = []
+    const prevHeader = this.headers[header.prevHashHex]
+    if (prevHeader) {
+      prevHeader.nextHash.push(header.hashHex)
+      header.processed = true
+    } else {
+      this.missedHeader = true
+    }
+    this.headers[header.hashHex] = header
+    this.processed = false
+    return header
+  }
+
+  process() {
+    if (this.processed) return
+    if (this.missedHeader) {
+      for (const hash in this.headers) {
+        const header = this.headers[hash]
+        const prevHeader = this.headers[header.prevHashHex]
+        if (!header.processed && prevHeader) {
+          prevHeader.nextHash.push(header.hashHex)
+          header.processed = true
+        }
+      }
+      this.missedHeader = false
+    }
+    let startingHash = this.genesisHash
+    let startingHeight = 0
+    if (this.maxReorgDepth > 0 && this.tip && !this.invalidatedBlock) {
+      startingHeight = Math.max(0, this.tip.height - this.maxReorgDepth)
+      const hash = this.chain[`${startingHeight}`]
+      if (this.headers[hash]) {
+        startingHash = hash
+        console.log(
+          `Processing starting at height: ${startingHeight} ${startingHash}`
+        )
+      } else {
+        console.log(`Missing height!: ${startingHash}`)
+      }
+    }
+    if (!this.headers[startingHash])
+      throw Error(`Missing starting block ${this.startingHash}`)
+    let hashes = [startingHash]
+    let longestChain
+    let height = startingHeight
+    while (hashes.length > 0) {
+      const nextHashes = []
+      for (const hash of hashes) {
+        const header = this.headers[hash]
+        if (header.height < height) {
+          console.log(`Detected loop in header ${height} ${hash}`)
+          continue // Loop. Ignore header
+        }
+        header.height = height
+        let added = 0
+        for (const hash of header.nextHash) {
+          if (this.headers[hash]) {
+            nextHashes.push(hash)
+            added++
+          }
+        }
+        if (added === 0) longestChain = header
+      }
+      hashes = nextHashes
+      height++
+    }
+    let indexHeader = longestChain
+    while (indexHeader) {
+      if (indexHeader.height < startingHeight) break
+      this.chain[`${indexHeader.height}`] = indexHeader.hashHex
+      indexHeader = this.headers[indexHeader.prevHashHex]
+    }
+    this.tip = longestChain
+    this.processed = true
+    this.invalidatedBlock = false
+  }
+
+  invalidateBlock(hash) {
+    hash = hash.toString('hex')
+    const header = this.headers[hash]
+    delete this.headers[hash]
+    if (header) {
+      console.log(`Invalidating block ${header.height}, ${hash}`)
+      const heightHash = this.chain[`${header.height}`]
+      if (header.hashHex === heightHash) {
+        for (let height = header.height; height <= this.tip.height; height++) {
+          const hash = this.chain[`${height}`]
+          console.log(`Invalidating block ${height}, ${hash}`)
+          delete this.chain[`${height}`]
+          delete this.headers[hash]
+        }
+      }
+    } else {
+      console.log(`Invalidating block ${hash}`)
+    }
+    this.invalidBlocks.add(hash)
+    this.processed = false
+    this.invalidatedBlock = true
+  }
+
+  getHeight(hash = false) {
+    this.process()
+    if (hash) {
+      hash = hash.toString('hex')
+      if (!this.headers[hash]) throw Error(`Missing header`)
+      return this.headers[hash].height
+    } else {
+      return this.getTip().height
+    }
+  }
+
+  getHash(height) {
+    this.process()
+    const hash = this.chain[`${height}`]
+    const header = this.headers[hash]
+    if (!header) throw Error(`Missing height`)
+    return header.hashHex
+  }
+
+  getHeader({ height = false, hash = false }) {
+    if (typeof height === 'number') hash = this.getHash(height)
+    const header = this.headers[hash]
+    if (!header) throw Error(`Missing header`)
+    return header
+  }
+
+  getTip() {
+    this.process()
+    if (!this.tip) throw Error(`No tip`)
+    const { hashHex: hash, height } = this.tip
+    return { hash, height }
+  }
+}
+
+module.exports = Headers
